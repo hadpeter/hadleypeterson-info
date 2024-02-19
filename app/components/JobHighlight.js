e88d439c69888dda37721e9858b5ca355a872e87
hadpeter <hadpeter@umich.edu>
2024-02-19 12:45:32 -0500

    styling and format for experience and blog pages

--- app/components/JobHighlight.js
+++ app/components/JobHighlight.js
@@ -3,7 +3,7 @@ export default function JobHighlight(props) {
     return (
         <div>
             <div className="text-xl font-bold">{props.title}, {props.focus}</div>
-            <div>{props.date}</div>
+            <div className="italic">{props.date}</div>
         </div>
     )
 }
--- app/components/blogPostPreview.js
+++ app/components/blogPostPreview.js
@@ -6,7 +6,7 @@ import Author from '../classes/author.js'
 export default function BlogPreview(props) {
   return (
     <div>
-      <div className="mb-5">
+      <div className="mb-5 hover:shadow-2xl transition-shadow duration-200">
         <CoverImage link={props.link} title={props.title} src={props.coverImage} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
@@ -17,7 +17,7 @@ export default function BlogPreview(props) {
           {props.title}
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div className="text-lg mb-4 italic">
         {props.date}
       </div>
       <p className="text-lg leading-relaxed mb-4">{props.excerpt} ...</p>
