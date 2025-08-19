d4283ecefdc0228c0b4163a527d5b586fdbfb7e8
hadpeter <hadpeter@umich.edu>
2025-08-18 23:32:22 -0400

    spacing

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -90,7 +90,7 @@ export default function Blogs() {
         }
     ]
     return (
-        <div className="min-h-screen my-6 mx-6">
+        <div className="min-h-screen my-6 mx-6 px-8">
             <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
             <PageTitleComponet title="Blogs"></PageTitleComponet>
             </section>
